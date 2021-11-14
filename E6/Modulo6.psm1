49593f4a97585f62c10c7b7f80e5d523b4212211
gerson-reyes24 <89707252+gerson-reyes24@users.noreply.github.com>
2021-11-14 14:06:47 -0600

    Add files via upload

---
+++ E6/Modulo6.psm1
@@ -0,0 +1,43 @@
+function Ver-StatusPerfil{  
+
+param([Parameter(Mandatory)] [ValidateSet("Public","Private")] [string] $perfil)  
+
+$status = Get-NetFirewallProfile -Name $perfil  
+
+Write-Host "Perfil:" $perfil  
+
+if($status.enabled){  
+
+Write-Host "Status: Activado"  
+
+} else{  
+
+Write-Host "Status: Desactivado"  
+
+}  
+
+}  
+
+function Ver-ReglasBloqueo{  
+
+if(Get-NetFirewallRule -Action Block -Enabled True -ErrorAction SilentlyContinue){  
+
+Get-NetFirewallRule -Action Block -Enabled True  
+
+} else{  
+
+Write-Host "No hay reglas definidas aún"  
+
+}  
+
+}
+
+function Ver-PerfilRedActual{  
+
+$perfilRed = Get-NetConnectionProfile  
+
+Write-Host "Nombre de red:" $perfilRed.Name  
+
+Write-Host "Perfil de red:" $perfilRed.NetworkCategory  
+
+}
